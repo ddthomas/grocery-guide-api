aff667d389a765ff48bf5012096eae2644a5c112
David Thomas <davidthomas@Davids-MacBook-Air.local>
2020-08-26 18:33:54 -0500

    add mongo and mongoose

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const express = require("express");
 const helmet = require("helmet");
 const compress = require("compression");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const db = require("./libs/conection");
+const Category = require("./models/category");
 const categories = require("./categories.json");
 
 const port = 3000;
@@ -28,10 +31,27 @@ app.get("/categories", (req, res) => {
 	res.json(categories);
 });
 
+app.get("/categories/new", (req, res) => {
+	const dairy = new Category({ categoryName: 'Dairy' });
+	console.log(dairy.name);
+	dairy.save(function (err) {
+		if (err) return console.error(err);
+		res.json(dairy);
+	  });	
+	
+});
+
+
+
 app.get("/test/:number", (req, res) => {
 	res.send("My number is: " + req.param("number"));
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
-});
+
+
+db.once('open', function() {
+	app.listen(port, () => {
+		console.log(`Example app listening at http://localhost:${port}`);
+	});
+	// we're connected!
+  });
---
+++ libs/conection.js
@@ -0,0 +1,9 @@
+
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb+srv://dbUser:${process.env.DB_PASSWORD}@cluster0.2tet7.mongodb.net/GroceryGuide?retryWrites=true&w=majority`, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+
+module.exports = db;
